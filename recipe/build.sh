a8154361bcbabee9f7bb24cdd40b5d286fec9e41
Isuru Fernando <isuruf@gmail.com>
2024-11-15 10:43:11 -0800

    fix cl build

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,12 @@ set -euxo pipefail
 
 _ARCH=${target_platform#*-} # keep chunk after dash (e.g. '64' in 'win-64')
 
+if [[ "${_ARCH}" == "64" ]]; then
+  _CL_MACHINE=x64
+elif [[ "${_ARCH}" == "arm64" ]]; then
+  _CL_MACHINE=ARM64
+fi
+
 # Build resources file
 test -f resources.rc && rm -f resources.rc
 echo "#include \"winuser.h\""      > resources.rc
@@ -37,8 +43,8 @@ for _TYPE in cli gui; do
   fi
 
   # You *could* use MSVC 2008 here, but you'd end up with much larger (~230k) executables.
-  if [[ "${_ARCH}" == "arm64" && "${c_compiler}" == "vc" ]]; then
-    cl.exe -opt:nowin98 -D NDEBUG -D "WIN32_LEAN_AND_MEAN" ${CPPFLAGS} -ZI -Gy -MT -MERGE launcher.c -Os -link -MACHINE:ARM64 ${LDFLAGS} resources-${_ARCH}.res version.lib advapi32.lib shell32.lib -out:${_TYPE}-${_ARCH}.exe
+  if [[ "${c_compiler}" == "vs" ]]; then
+    cl.exe -opt:nowin98 -D NDEBUG -D "WIN32_LEAN_AND_MEAN" ${CPPFLAGS} -ZI -Gy -MT -MERGE launcher.c -Os -link -MACHINE:${_CL_MACHINE} ${LDFLAGS} resources-${_ARCH}.res version.lib advapi32.lib shell32.lib -out:${_TYPE}-${_ARCH}.exe
   else
     ${CC} -O2 -DSCRIPT_WRAPPER -DUNICODE -D_UNICODE -DMINGW_HAS_SECURE_API -DMAXINT=INT_MAX ${CPPFLAGS} \
       ${SRC_DIR}/launcher.c -c -o ${_TYPE}-${_ARCH}.o
