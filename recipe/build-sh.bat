e8cbd08bca6c1b2b653940eafa95364168f7b09d
jaimergp <jaimergp@users.noreply.github.com>
2026-05-04 14:07:04 +0200

    Revert

--- recipe/build-sh.bat
+++ recipe/build-sh.bat
@@ -13,7 +13,6 @@ IF %ERRORLEVEL% NEQ 0 exit 1
 
 :: Delegate to the Unixy script. We need to translate the key path variables
 :: to be Unix-y rather than Windows-y, though.
-set "OG_PREGIX=!PREFIX!"
 FOR /F "delims=" %%i IN ('cygpath.exe -u -p "%PATH%"') DO set "PATH_OVERRIDE=%%i"
 FOR /F "delims=" %%i IN ('cygpath.exe -u "%PREFIX%"') DO set "PREFIX=%%i"
 FOR /F "delims=" %%i in ('cygpath.exe -u "%BUILD_PREFIX%"') DO set "BUILD_PREFIX=%%i"
@@ -21,12 +20,3 @@ FOR /F "delims=" %%i in ('cygpath.exe -u "%BUILD_PREFIX%"') DO set "BUILD_PREFIX
 copy %RECIPE_DIR%\build.sh .
 bash build.sh
 IF %ERRORLEVEL% NEQ 0 exit 1
-
-echo "OG_PREFIX:"
-echo %OG_PREFIX%
-echo "PREFIX:"
-echo %PREFIX%
-echo dir OG_PREFIX
-dir "%OG_PREFIX%"
-echo dir PREFIX
-dir "%PREFIX%"
